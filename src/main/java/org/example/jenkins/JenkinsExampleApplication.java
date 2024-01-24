70f7ceba2394b6a46daf081e444a8f6fcfdf4d74
Nonelela Cele <nonelela.cele@s4.co.za>
2024-01-24 14:41:31 +0200

    first commit

---
+++ src/main/java/org/example/jenkins/JenkinsExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsExampleApplication.class, args);
+	}
+
+}
